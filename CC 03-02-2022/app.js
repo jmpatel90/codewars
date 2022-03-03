3bd7d8133d9ed1030338fca7ffaa30725557b688
Jay Patel <jmpatel90@gmail.com>
2022-03-02 23:02:05 -0500

    Added attempt  at 3-3 coding challenge

---
+++ CC 03-02-2022/app.js
@@ -0,0 +1,24 @@
+// Convert number to reversed array of digits
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+// Example(Input => Output):
+// 348597 => [7,9,5,8,4,3]
+// 0 => [0]
+
+// P: Are you always given digits?
+// R: Whatever number is given, it's now split up into an array and the order of the numbers is reversed
+// E: If given 348597 => [7,9,5,8,4,3]
+//    If given 123456 => [6,5,4,3,2,1]
+//    If given 494096 => [6,9,0,4,9,4]
+// P: Make a function that takes in a number. 
+function  reverseArray(num){
+    // make number a string and split it and reverse array
+    let numArray = num.toString().split('').reverse()
+    // return 
+    return Number(...numArray)
+}
+
+//Test cases
+reverseArray(123)
+console.log(reverseArray(456), [6,5,4])
+// This doesn't work. Will have to use map.
